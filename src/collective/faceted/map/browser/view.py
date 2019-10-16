cb0fff4aee805b638ab476bbd439529d6d289cf4
Martin Peeters <martin.peeters@affinitic.be>
2019-10-16 16:48:21 +0200

    Add a view to generate json map markers

---
+++ src/collective/faceted/map/browser/view.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from Products.Five import BrowserView
+from plone import api
+
+import json
+
+
+class FacetedGeoJSON(BrowserView):
+    def data_geo(self, batch):
+        """Return the json data for the given batch"""
+        elements = [self._generate_point(brain) for brain in batch]
+        return json.dumps(
+            {"type": "FeatureCollection", "features": [e for e in elements if e]}
+        )
+
+    def _generate_point(self, brain):
+        catalog = api.portal.get_tool(name="portal_catalog")
+        data = catalog.getIndexDataForRID(brain.getRID())
+        if data.get("longitude") and data.get("latitude"):
+            return {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [data["longitude"], data["latitude"]],
+                },
+                "id": brain.id,
+                "properties": {
+                    "popup": "<a href='{0}' title='{1}'>{1}</a>".format(
+                        brain.getURL(),
+                        brain.Title,
+                    )
+                },
+            }
