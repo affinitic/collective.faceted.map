6ef01ebfd1dfe5189c7fd6dc48a8dd29aefbeac0
Martin Peeters <martin.peeters@affinitic.be>
2019-10-18 20:17:44 +0200

    Avoid to return an empty list of features that breaks Leaflet

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,7 +5,8 @@ Changelog
 1.0a2 (unreleased)
 ------------------
 
-- Nothing changed yet.
+- Avoid to return an empty list of features that breaks Leaflet
+  [mpeeters]
 
 
 1.0a1 (2019-10-16)
--- src/collective/faceted/map/browser/view.py
+++ src/collective/faceted/map/browser/view.py
@@ -10,9 +10,10 @@ class FacetedGeoJSON(BrowserView):
     def data_geo(self, batch):
         """Return the json data for the given batch"""
         elements = [self._generate_point(brain) for brain in batch]
-        return json.dumps(
-            {"type": "FeatureCollection", "features": [e for e in elements if e]}
-        )
+        features = [e for e in elements if e]
+        if not features:
+            return u""
+        return json.dumps({"type": "FeatureCollection", "features": features})
 
     def _generate_point(self, brain):
         catalog = api.portal.get_tool(name="portal_catalog")
