f69f907ba6a80b7312621b5fbd941efc7556d703
Martin Peeters <martin.peeters@affinitic.be>
2020-07-16 09:47:36 +0200

    Back to development: 1.0b6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.faceted.map",
-    version="1.0b5",
+    version="1.0b6.dev0",
     description="Map view for eea.facetednavigation",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
